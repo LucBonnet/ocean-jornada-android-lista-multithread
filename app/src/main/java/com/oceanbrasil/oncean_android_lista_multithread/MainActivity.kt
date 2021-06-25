4b08f513745b66a52e8b451d7f166becaaccadfd
Luc <luc@Jelubo.local>
2021-06-25 10:20:51 -0300

    first commit

---
+++ app/src/main/java/com/oceanbrasil/oncean_android_lista_multithread/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.oncean_android_lista_multithread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
